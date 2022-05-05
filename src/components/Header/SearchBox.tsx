cf107fa6ee9bdc66a92cf8b94fc5f8143b539bfd
Vitor Niehues <niehues.vitor@gmail.com>
2022-05-05 18:23:30 -0300

    Add search by typing

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -2,89 +2,115 @@ import { Flex, Icon, Input, List, ListItem, Modal, ModalBody, ModalCloseButton, 
 import { useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
+interface CustomListItemProps {
+  id: number,
+  title: string,
+}
+
 export function SearchBox() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [name, setName] = useState('Presidente Combustíveis e Derivados de Petróleo Ltda')
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [name, setName] = useState('Presidente Combustíveis e Derivados de Petróleo Ltda')
 
+  const constList = [
+    { id: 1, title: "Comercial de Combustíveis Presidente Ltda" },
+    { id: 2, title: "Combustíveis Presidente Ltda" },
+    { id: 3, title: "Presidente Combustíveis e Derivados de Petróleo Ltda" },
+    { id: 4, title: "Posto Presidente" },
+  ]
+  const [list, setList] = useState<CustomListItemProps[]>(constList)
+
+  function CustomListItem({ id, title }: CustomListItemProps) {
     return (
-        <Flex
-            as="label"
-            flex="1"
-            py="4"
-            px="8"
-            ml="6"
-            maxW={500}
-            alignSelf="center"
-            alignItems="center"
-            color="gray.200"
-            position="relative"
-            bg="gray.800"
-            borderRadius="full"
-            onClick={onOpen}
-            cursor="pointer"
-        >
-            <Text>
-                {name}
-            </Text>
-            <Spacer />
-
-            <Icon as={RiSearchLine} fontSize="20" />
-            <Modal
-                isOpen={isOpen}
-                onClose={onClose}
-                scrollBehavior="inside"
-                size="lg"
-            >
-                <ModalOverlay />
-                <ModalContent bg="gray.800">
-                    <ModalHeader>
-                        <Input
-                            color="gray.50"
-                            variant="unstyled"
-                            px="4"
-                            mr="2"
-                            placeholder="Digite para buscar"
-                            _placeholder={{ color: "gray.400" }}
-                        />
-                        <ModalCloseButton />
-                    </ModalHeader>
-                    <ModalBody pt="0">
-                        <List borderTop="gray.50" borderTopWidth="thin" pt="4" spacing="2">
-                            <CustomListItem texto="Presidente Combustíveis e Derivados de Petróleo Ltda" />
-                            <CustomListItem texto="Combustíveis Presidente Ltda" />
-                            <CustomListItem texto="Empresa 03" />
-                            <CustomListItem texto="Empresa 04" />
-                            <CustomListItem texto="Empresa 05" />
-                            <CustomListItem texto="Empresa 06" />
-                            <CustomListItem texto="Empresa 07" />
-                            <CustomListItem texto="Empresa 08" />
-                            <CustomListItem texto="Empresa 09" />
-                            <CustomListItem texto="Empresa 10" />
-                        </List>
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        </Flex>
+      <ListItem
+        as="button"
+        p="4"
+        bg="gray.700"
+        borderRadius="md"
+        w="100%"
+        textAlign="start"
+        _hover={{ bg: "gray.500" }}
+        onClick={() => handleListOnClick({ id, title })}
+      >
+        {title}
+      </ListItem>
     )
-}
+  }
 
-function CustomListItem({ texto }: { texto: string }) {
-    return (
-        <ListItem
-            as="button"
-            p="4"
-            bg="gray.700"
-            borderRadius="md"
-            w="100%"
-            textAlign="start"
-            _hover={{ bg: "gray.500" }}
-            onClick={handleListOnClick}
-        >
-            {texto}
-        </ListItem>
+  function handleListOnClick({ id, title }: CustomListItemProps) {
+    setName(title)
+    onClose()
+  }
+
+  function handleInputOnChage(value: string): void {
+    const newList = constList.filter((e) =>
+      e.title
+        .toLowerCase()
+        .includes(value.toLowerCase())
     )
+    setList(newList)
+  }
+
+  function handleOpenModal(): void {
+    setList(constList)
+    onOpen()
+  }
+
+  return (
+    <Flex
+      as="label"
+      flex="1"
+      py="4"
+      px="8"
+      ml="6"
+      maxW={500}
+      alignSelf="center"
+      alignItems="center"
+      color="gray.200"
+      position="relative"
+      bg="gray.800"
+      borderRadius="full"
+      onClick={handleOpenModal}
+      cursor="pointer"
+    >
+      <Text>
+        {name}
+      </Text>
+      <Spacer />
+
+      <Icon as={RiSearchLine} fontSize="20" />
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        scrollBehavior="inside"
+        size="lg"
+      >
+        <ModalOverlay />
+        <ModalContent bg="gray.800">
+          <ModalHeader>
+            <Input
+              color="gray.50"
+              variant="unstyled"
+              px="4"
+              mr="2"
+              placeholder="Digite para buscar"
+              _placeholder={{ color: "gray.400" }}
+              onChange={(e) => handleInputOnChage(e.target.value)}
+            />
+            <ModalCloseButton />
+          </ModalHeader>
+          <ModalBody pt="0" pb="4">
+            <List borderTop="gray.50" borderTopWidth="thin" pt="4" spacing="2">
+              {
+                list.map(({ id, title }) => {
+                  return (<CustomListItem key={id} id={id} title={title} />)
+                })
+              }
+            </List>
+
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </Flex>
+  )
 }
 
-function handleListOnClick() {
-    console.log("teste")
-}
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,20 +1,20 @@
 import { Box, Stack } from "@chakra-ui/react";
-import { RiContactsLine, RiDashboardLine, RiInputMethodLine, RiMoneyDollarCircleFill } from "react-icons/ri";
+import { RiContactsLine, RiDashboardLine, RiMoneyDollarCircleFill } from "react-icons/ri";
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
 
 export function Sidebar() {
-    return (
-        <Box as="aside" w="auto" minW="32" mr="8">
-            <Stack spacing="12" align="flex-start">
-                <NavSection title="CONSULTAS">
-                    <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
-                    <NavLink icon={RiMoneyDollarCircleFill} href="/precos">Preços</NavLink>
-                </NavSection>
-                <NavSection title="CADASTROS">
-                    <NavLink icon={RiContactsLine} href="/users">Usuários</NavLink>
-                </NavSection>
-            </Stack>
-        </Box>
-    )
+  return (
+    <Box as="aside" w="auto" minW="32" mr="8">
+      <Stack spacing="12" align="flex-start">
+        <NavSection title="CONSULTAS">
+          <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
+          <NavLink icon={RiMoneyDollarCircleFill} href="/precos">Preços</NavLink>
+        </NavSection>
+        <NavSection title="CADASTROS">
+          <NavLink icon={RiContactsLine} href="/users">Usuários</NavLink>
+        </NavSection>
+      </Stack>
+    </Box>
+  )
 }
