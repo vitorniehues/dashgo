109ed2f3412760f465ac11d6c24c9a80f969df02
Vitor Niehues <niehues.vitor@gmail.com>
2022-05-11 18:29:07 -0300

    Merge branch 'main' of https://github.com/vitorniehues/dashgo into main

--- src/components/ProductsTable/ProductTabPanel.tsx
+++ src/components/ProductsTable/ProductTabPanel.tsx
@@ -1,29 +1,34 @@
-import { TabPanel, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
+import { TabPanel, Table, Thead, Tr, Th, Tbody, Td, Heading, Tfoot, Text, Box } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { ProductTabPanelItem } from "./ProductTabPanelItem";
 
 interface ProductTablePanelProps {
-    children: ReactNode
+  children: ReactNode
 }
 
 export function ProductTabPanel({ children, ...rest }: ProductTablePanelProps) {
-    return (
-        <TabPanel {...rest}>
-            <Table>
-                <Thead>
-                    <Tr>
-                        <Th>
-                            Forma de Pagamento
-                        </Th>
-                        <Th isNumeric>
-                            Preço
-                        </Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {children}
-                </Tbody>
-            </Table>
-        </TabPanel>
-    )
+  return (
+    <TabPanel {...rest}>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>
+              Forma de Pagamento
+            </Th>
+            <Th isNumeric>
+              Preço
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {children}
+        </Tbody>
+        <Tfoot >
+          <Box mt="6">
+            <Text fontSize="xs" fontWeight="normal" >Sujeito a alteração sem aviso prévio.</Text>
+          </Box>
+        </Tfoot>
+      </Table>
+    </TabPanel>
+  )
 }
--- src/pages/precos/index.tsx
+++ src/pages/precos/index.tsx
@@ -5,43 +5,42 @@ import { ProductTab } from "../../components/ProductsTable/ProductTab";
 import { ProductTabPanelItem } from "../../components/ProductsTable/ProductTabPanelItem";
 
 export default function ProductsList() {
-    return (
+  return (
 
-        <BasePage>
-            <Box flex="1" bg="gray.800" p="8">
-                <Flex mb="8" justify="space-between" align="center">
-                    <Heading size="lg" fontWeight="normal" >Produtos</Heading>
-                    <Text fontSize="xs" fontWeight="normal" >Sujeito a alteração sem aviso prévio.</Text>
-                </Flex>
-                <Tabs variant="solid-rounded" size="md" isFitted>
-                    <TabList>
-                        <ProductTab>Gasolina Comum</ProductTab>
-                        <ProductTab>Gasolina Aditivada</ProductTab>
-                        <ProductTab>Diesel S500</ProductTab>
-                        <ProductTab>Diesel S10</ProductTab>
-                    </TabList>
-                    <TabPanels>
-                        <ProductTabPanel >
-                            <ProductTabPanelItem description="Dinheiro" price={5.50} />
-                            <ProductTabPanelItem description="TEF Crédito" price={5.55} />
-                            <ProductTabPanelItem description="Nota a Cobrar" price={5.60} />
-                        </ProductTabPanel>
-                        <ProductTabPanel >
-                            <ProductTabPanelItem description="Dinheiro" price={5.51} />
-                            <ProductTabPanelItem description="TEF Frota" price={5.56} />
-                            <ProductTabPanelItem description="Pgto com crédito de antecipação" price={5.61} />
-                        </ProductTabPanel>
-                        <ProductTabPanel >
-                            <ProductTabPanelItem description="" price={4.50} />
-                        </ProductTabPanel>
-                        <ProductTabPanel >
-                            <ProductTabPanelItem description="Dinheiro" price={4.60} />
-                            <ProductTabPanelItem description="TEF Débito" price={4.65} />
-                            <ProductTabPanelItem description="Cheque pré-datato (30 dias)" price={4.70} />
-                        </ProductTabPanel>
-                    </TabPanels>
-                </Tabs>
-            </Box>
-        </BasePage >
-    )
+    <BasePage>
+      <Box flex="1" bg="gray.800" p="8">
+        <Flex mb="4" py="2" justify="space-between" align="center">
+          <Heading size="lg" fontWeight="normal" >Produtos</Heading>
+        </Flex>
+        <Tabs variant="solid-rounded" size="md" isFitted>
+          <TabList>
+            <ProductTab>Gasolina Comum</ProductTab>
+            <ProductTab>Gasolina Aditivada</ProductTab>
+            <ProductTab>Diesel S500</ProductTab>
+            <ProductTab>Diesel S10</ProductTab>
+          </TabList>
+          <TabPanels>
+            <ProductTabPanel >
+              <ProductTabPanelItem description="Dinheiro" price={5.50} />
+              <ProductTabPanelItem description="TEF Crédito" price={5.55} />
+              <ProductTabPanelItem description="Nota a Cobrar" price={5.60} />
+            </ProductTabPanel>
+            <ProductTabPanel >
+              <ProductTabPanelItem description="Dinheiro" price={5.51} />
+              <ProductTabPanelItem description="TEF Frota" price={5.56} />
+              <ProductTabPanelItem description="Pgto com crédito de antecipação" price={5.61} />
+            </ProductTabPanel>
+            <ProductTabPanel >
+              <ProductTabPanelItem description="" price={4.50} />
+            </ProductTabPanel>
+            <ProductTabPanel >
+              <ProductTabPanelItem description="Dinheiro" price={4.60} />
+              <ProductTabPanelItem description="TEF Débito" price={4.65} />
+              <ProductTabPanelItem description="Cheque pré-datato (30 dias)" price={4.70} />
+            </ProductTabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </BasePage >
+  )
 }
